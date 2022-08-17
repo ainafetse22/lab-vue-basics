04af215961174dbe5068bb027c0f9d4d0e6e581c
Estefania A <zestefania.amundaray@gmail.com>
2022-08-18 00:30:43 +0200

    It1 y 2

---
+++ vue-basic/src/components/FooterDiv.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <footer>{{msg}}</footer>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() { // needed when the component is no SFC
+    const msg = ref('Footer Msg');
+    return { msg }; // combined with setup()
+  },
+};
+
+</script>
+
+<style>
+
+</style>
---
+++ vue-basic/src/components/FuncMsg.vue
@@ -0,0 +1,16 @@
+<template>
+<H1>{{showMsg}}</H1>
+</template>
+
+<script setup> // exposed everything to the template, no return needed for var and functions
+import { ref, onMounted } from 'vue';
+
+const showMsg = ref('initial message');
+onMounted(() => {
+  showMsg.value = ref('Hello from mounted function'); // <div>
+});
+
+</script>
+<style>
+
+</style>
