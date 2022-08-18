ec7ab424298fbe26ed9e64be78ffef4ac7653f18
Estefania A <zestefania.amundaray@gmail.com>
2022-08-18 21:28:03 +0200

    iteration 3 done

--- vue-basic/src/components/FuncMsg.vue
+++ vue-basic/src/components/FuncMsg.vue
@@ -1,16 +1,29 @@
 <template>
-<H1>{{showMsg}}</H1>
+<h1>{{showMsg}}</h1>
+<p v-show="boolShow">This p is static but shows with V-IF</p>
+<img v-if="boolShow" class="img-logo" src="../assets/logo.png" alt="logo">
+<button @click="changeShow">ShowSelector</button>
+<slot></slot>
 </template>
 
 <script setup> // exposed everything to the template, no return needed for var and functions
 import { ref, onMounted } from 'vue';
 
 const showMsg = ref('initial message');
+const boolShow = ref(true);
+
+const changeShow = () => {
+  console.log(boolShow);
+  boolShow.value = !boolShow.value;
+};
+
 onMounted(() => {
   showMsg.value = ref('Hello from mounted function'); // <div>
 });
 
 </script>
 <style>
-
+.img-logo{
+  filter: grayscale(100%);
+}
 </style>
--- vue-basic/src/views/HomeView.vue
+++ vue-basic/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <FuncMsg></FuncMsg>
+    <FuncMsg>
+      <h2>This is injected from the parent</h2>
+    </FuncMsg>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
